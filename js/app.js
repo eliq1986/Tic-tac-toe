955cfcd2bd5a7a2f8606b656e7f2b2d5efe9669c
Elijah Quesada <this_boy10@yahoo.com>
2018-06-27 10:51:19 -0700

    basic functionality completed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 //DOM ELEMENTS
 const startScreen = document.getElementById("start");
 const boardScreen = document.getElementById("board");
@@ -9,41 +8,47 @@ const boxes = document.querySelectorAll(".box");
 
 
 
-
 //hides board and finish screen
-const hideHideShow = function(hideScreen,hideScreen2,displayScreen) {
+const hideHideShow = function(hideScreen,hideScreen2,showScreen) {
    hideScreen.style.display = "none";
    hideScreen2.style.display = "none";
-   displayScreen.style.display = "block";
+   showScreen.style.display = "block";
  }
 
 //hides on window load and displays start screen
-window.onload = hideHideShow(boardScreen,finishScreen,startScreen);
+hideHideShow(boardScreen,finishScreen,startScreen);
 
 
 //Starts game
 start.addEventListener("click", event => {
-   hideHideShow(finishScreen,startScreen,boardScreen);
-   player1.isTurn ? player1ListItem.className = "players active" : null
+      event.target.textContent === "Start game"? hideHideShow(finishScreen,startScreen,boardScreen):null;
+      player1.isTurn ? player1ListItem.className = "players active" : null
 });
 
-//Player template
+
+
+//PLAYER TEMPLATE
 class Player  {
   constructor(name,isTurn,playerLi) {
     this.name = name,
     this.isTurn = isTurn,
     this.playerLi = playerLi
 
+
     }
     setActive() {
      if (this.isTurn) {
        const player = document.getElementById(this.playerLi);
        player.classList.add("active");
+
      } else {
+
        const player = document.getElementById(this.playerLi);
        player.classList.remove("active");
+
      }
     }
+
 
  set turn(isTurn) {
    this.isTurn = isTurn;
@@ -55,13 +60,60 @@ class Player  {
 
 // BOARD
 class Board {
-  constructor(status) {
-  this.status = status
+  constructor(status,player1WinOrLoose, player2WinOrLoose) {
+  this.status = status,
+  this.player1 = player1WinOrLoose,
+  this.player2 = player1WinOrLoose
  }
+
+  get isGameFinished() {
+      if(this.status === "player1") {
+       hideHideShow(boardScreen,startScreen,finishScreen);
+       finishScreen.className = "screen screen-win-one";
+       this.status = "start";
+
+     } else if (this.status === "player2") {
+       hideHideShow(boardScreen,startScreen,finishScreen);
+       finishScreen.className = "screen screen-win-two";
+       this.status = "start";
+     }
+
+  }
   get stat() {
      const getBoxes = [...document.querySelectorAll(".box")];
-     const winnerO = getBoxes.filter(box => box.className === "box box-filled-1");
-     const winnerX = getBoxes.filter(box => box.className === "box box-filled-2");
+       if (getBoxes[0].className === "box box-filled-1" && getBoxes[1].className === "box box-filled-1" & getBoxes[2].className === "box box-filled-1") {
+              this.status = "player1";
+
+       }else if (getBoxes[0].className === "box box-filled-1" && getBoxes[3].className === "box box-filled-1" & getBoxes[6].className === "box box-filled-1") {
+              this.status = "player1";
+       } else if (getBoxes[0].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[8].className === "box box-filled-1") {
+              this.status = "player1";
+       }
+       else if (getBoxes[1].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[7].className === "box box-filled-1") {
+              this.status = "player1";
+       } else if (getBoxes[2].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[6].className === "box box-filled-1") {
+              this.status = "player1";
+       } else if (getBoxes[3].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[5].className === "box box-filled-1") {
+              this.status = "player1";
+       } else if (getBoxes[6].className === "box box-filled-1" && getBoxes[7].className === "box box-filled-1" & getBoxes[8].className === "box box-filled-1") {
+              this.status = "player1";
+       }
+         else if (getBoxes[0].className === "box box-filled-2" && getBoxes[1].className === "box box-filled-2" & getBoxes[2].className === "box box-filled-2") {
+         this.status = "player2";
+       }else if (getBoxes[0].className === "box box-filled-2" && getBoxes[3].className === "box box-filled-2" & getBoxes[6].className === "box box-filled-2") {
+        this.status = "player2";
+       } else if (getBoxes[0].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[8].className === "box box-filled-2") {
+         this.status = "player2";
+       }
+       else if (getBoxes[1].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[7].className === "box box-filled-2") {
+         this.status = "player2";
+       } else if (getBoxes[2].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[6].className === "box box-filled-2") {
+          this.status = "player2";
+       } else if (getBoxes[3].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[5].className === "box box-filled-2") {
+          this.status = "player2";
+       } else if (getBoxes[6].className === "box box-filled-2" && getBoxes[7].className === "box box-filled-2" & getBoxes[8].className === "box box-filled-2") {
+         this.status = "player2";
+       }
 
 
 }
@@ -73,14 +125,30 @@ const player1 = new Player("Player1", true, "player1" );
 const player2 = new Player("Player 2", false, "player2");
 const gameBoard = new Board("start");
 
+finishScreen.addEventListener("click", event => {
+  
+   if (event.target.textContent === "New game") {
+     let resetBoard = [...document.querySelectorAll(".box")];
+      resetBoard.forEach(box => {
+        box.classList.remove("box-filled-2");
+        box.classList.remove("box-filled-1");
+
+      });
+
+   }
+   hideHideShow(finishScreen,startScreen,boardScreen);
+
+});
+
+
 box.addEventListener("mouseover", (event)=> {
 
  if (event.target.className === "box box-filled-1" || event.target.className === "box box-filled-2" ) {
-    player1.isTurn ? event.target.style.backgroundImage = "" : null
+    player1.isTurn ? event.target.style.backgroundImage = "" : null;
     player2.isTurn ? event.target.style.backgroundImage = "" : null;
  }
    else {
-         player1.isTurn ? event.target.style.backgroundImage = "url('img/o.svg')" : null
+         player1.isTurn ? event.target.style.backgroundImage = "url('img/o.svg')" : null;
          player2.isTurn ? event.target.style.backgroundImage = "url('img/x.svg')" : null;
        }
 
@@ -100,19 +168,19 @@ box.addEventListener("click", (event) => {
 
       if(player1.isTurn === true
         && event.target.className !== "box box-filled-1"
-        && event.target.className !== "box box-filled-2" ) {
-
-         event.target.classList.add("box-filled-1");
-         player2.turn = true;
+        && event.target.className !== "box box-filled-2" )
+        {
+        event.target.classList.add("box-filled-1");
          player1.turn = false;
+         player2.turn = true;
          player1.setActive();
           player2.setActive();
+        }
 
-      }
         else if (player2.isTurn === true
           && event.target.className !== "box box-filled-2"
-          && event.target.className !== "box box-filled-1") {
-
+          && event.target.className !== "box box-filled-1")
+          {
           event.target.classList.add("box-filled-2");
           player2.turn = false;
           player1.turn = true;
@@ -121,4 +189,5 @@ box.addEventListener("click", (event) => {
 
         }
         gameBoard.stat;
+        gameBoard.isGameFinished;
 });
