6ea53bc62cad6765ec82015b05b668760392218e
Elijah Quesada <this_boy10@yahoo.com>
2018-06-28 08:30:18 -0700

    Basic Completed

--- js/app.js
+++ js/app.js
@@ -5,6 +5,31 @@ const finishScreen = document.getElementById("finish");
 const player1ListItem = document.getElementById("player1");
 const box = document.querySelector(".boxes");
 const boxes = document.querySelectorAll(".box");
+const firstRow = [...document.querySelectorAll(".box")].splice(0,3);
+const secondRow = [...document.querySelectorAll(".box")].splice(3,3);
+const thirdRow = [...document.querySelectorAll(".box")].splice(6);
+let firstRowDown = [];
+let secondRowDown = [];
+let thirdRowDown = [];
+let diagonal1 = [];
+let diagonal2 = [];
+
+   for (let i = 0; i<boxes.length; i+=4) {
+      diagonal1.push(boxes[i]);
+    }
+    for (let i = 2; i<7; i+=2) {
+      diagonal2.push(boxes[i]);
+    }
+    for (let i = 0; i<7; i+=3) {
+      firstRowDown.push(boxes[i]);
+    }
+    for (let i  = 1; i<8; i+= 3) {
+      secondRowDown.push(boxes[i]);
+    }
+    for (let i  = 2; i<9; i+= 3) {
+      thirdRowDown.push(boxes[i]);
+    }
+
 
 //hides board and finish screen
 const hideHideShow = function(hideScreen,hideScreen2,showScreen) {
@@ -13,17 +38,14 @@ const hideHideShow = function(hideScreen,hideScreen2,showScreen) {
    showScreen.style.display = "block";
 
  }
-
 //hides on window load and displays start screen
 hideHideShow(boardScreen,finishScreen,startScreen);
 
 //Starts game
 start.addEventListener("click", event => {
-
       event.target.textContent === "Start game" ? hideHideShow(finishScreen,startScreen,boardScreen):null;
       player1.isTurn ? player1ListItem.className = "players active" : null;
 });
-
 
 //PLAYER TEMPLATE
 class Player  {
@@ -46,17 +68,14 @@ class Player  {
    set turn(isTurn) {
      this.isTurn = isTurn;
  }
-
-
 }
-// BOARD
+
 class Board {
-  constructor(status,boxCount,topRowWin,middleRowWin,lowerRowWin) {
+  constructor(status,boxCount,topRow) {
   this.status = status,
-  this.boxCount = boxCount,
-  this.topRowWin = topRowWin,
-  this.middleRowWin = middleRowWin,
-  this.lowerRowWin = lowerRowWin;
+  this.boxCount = boxCount
+
+
  }
 
   get isGameFinished() {
@@ -92,49 +111,38 @@ class Board {
       }
 
   }
-
-  get stat() {
-     const getBoxes = [...document.querySelectorAll(".box")];
-
-       if (getBoxes[0].className === "box box-filled-1" && getBoxes[1].className === "box box-filled-1" & getBoxes[2].className === "box box-filled-1") {
-              this.status = "player1";
-
-       }else if (getBoxes[0].className === "box box-filled-1" && getBoxes[3].className === "box box-filled-1" & getBoxes[6].className === "box box-filled-1") {
-              this.status = "player1";
-       } else if (getBoxes[0].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[8].className === "box box-filled-1") {
-              this.status = "player1";
-       }
-       else if (getBoxes[1].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[7].className === "box box-filled-1") {
-              this.status = "player1";
-       } else if (getBoxes[2].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[6].className === "box box-filled-1") {
-              this.status = "player1";
-       } else if (getBoxes[3].className === "box box-filled-1" && getBoxes[4].className === "box box-filled-1" & getBoxes[5].className === "box box-filled-1") {
-              this.status = "player1";
-       } else if (getBoxes[6].className === "box box-filled-1" && getBoxes[7].className === "box box-filled-1" & getBoxes[8].className === "box box-filled-1") {
-              this.status = "player1";
-       }
-         else if (getBoxes[0].className === "box box-filled-2" && getBoxes[1].className === "box box-filled-2" & getBoxes[2].className === "box box-filled-2") {
-         this.status = "player2";
-       }else if (getBoxes[0].className === "box box-filled-2" && getBoxes[3].className === "box box-filled-2" & getBoxes[6].className === "box box-filled-2") {
-        this.status = "player2";
-       } else if (getBoxes[0].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[8].className === "box box-filled-2") {
-         this.status = "player2";
-       }
-       else if (getBoxes[1].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[7].className === "box box-filled-2") {
-         this.status = "player2";
-       } else if (getBoxes[2].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[6].className === "box box-filled-2") {
-          this.status = "player2";
-       } else if (getBoxes[3].className === "box box-filled-2" && getBoxes[4].className === "box box-filled-2" & getBoxes[5].className === "box box-filled-2") {
-          this.status = "player2";
-       } else if (getBoxes[6].className === "box box-filled-2" && getBoxes[7].className === "box box-filled-2" & getBoxes[8].className === "box box-filled-2") {
-         this.status = "player2";
-       }
-}
       set counter(boxCount) {
         this.boxCount += boxCount;
 }
+   get boardStatus() {
+           let thirdRowD = thirdRowDown.filter(box => box.className === "box box-filled-2");
+           let secondRowD = secondRowDown.filter(box => box.className === "box box-filled-2");
+           let rowOne = firstRow.filter(box => box.className === "box box-filled-2");
+           let rowTwo = secondRow.filter(box => box.className === "box box-filled-2");
+           let rowThree = thirdRow.filter(box => box.className === "box box-filled-2");
+           let dia1 = diagonal1.filter(box => box.className === "box box-filled-2");
+           let dia2 = diagonal2.filter(box => box.className === "box box-filled-2");
+           let firstRowD = firstRowDown.filter(box => box.className === "box box-filled-2");
 
-}
+          if (rowOne.length === 3 || rowTwo.length === 3 || rowThree.length === 3 || dia1.length ===3 || dia2.length === 3 || firstRowD.length === 3 || secondRowD.length === 3 || thirdRowD.length === 3) {
+            this.status = "player2";
+              }
+              let thirdRowDownCirlce = thirdRowDown.filter(box => box.className === "box box-filled-2");
+              let secondRowDownCircle = secondRowDown.filter(box => box.className === "box box-filled-1");
+              let firstRowDCircle = firstRowDown.filter(box => box.className === "box box-filled-1");
+              let rowOneCircle = firstRow.filter(box => box.className === "box box-filled-1");
+              let rowTwoCircle = secondRow.filter(box => box.className === "box box-filled-1");
+              let rowThreeCircle = thirdRow.filter(box => box.className === "box box-filled-1");
+              let dia1Circle = diagonal1.filter(box => box.className === "box box-filled-1");
+              let dia2Circle = diagonal2.filter(box => box.className === "box box-filled-1");
+
+             if (rowOneCircle.length === 3 || rowTwoCircle.length === 3 || rowThreeCircle.length === 3 || dia1Circle.length === 3 || dia2Circle.length === 3 || firstRowDCircle.length === 3||
+                 secondRowDownCircle.length === "box box-filled-1" || thirdRowDownCirlce.length === 3) {
+              this.status = "player1";
+          }
+       }
+
+   }
 
 const player1 = new Player("Player1", true, "player1" );
 const player2 = new Player("Player 2", false, "player2");
@@ -143,8 +151,7 @@ const gameBoard = new Board("start", 0);
 finishScreen.addEventListener("click", event => {
 
    if (event.target.textContent === "New game") {
-     let resetBoard = [...boxes];
-      resetBoard.forEach(box => {
+      boxes.forEach(box => {
         box.classList.remove("box-filled-2");
         box.classList.remove("box-filled-1");
   });
@@ -176,11 +183,8 @@ box.addEventListener("mouseout", (event) => {
 
 box.addEventListener("click", (event) => {
 
-      if(player1.isTurn === true
-        && event.target.className !== "box box-filled-1"
-        && event.target.className !== "box box-filled-2" )
-        {
-        event.target.classList.add("box-filled-1");
+      if(player1.isTurn === true  && event.target.className !== "box box-filled-1" && event.target.className !== "box box-filled-2" ) {
+         event.target.classList.add("box-filled-1");
          player1.turn = false;
          player2.turn = true;
          gameBoard.boxCount += 1;
@@ -188,10 +192,7 @@ box.addEventListener("click", (event) => {
           player2.setActive();
         }
 
-        else if (player2.isTurn === true
-          && event.target.className !== "box box-filled-2"
-          && event.target.className !== "box box-filled-1")
-          {
+        else if (player2.isTurn === true && event.target.className !== "box box-filled-2" && event.target.className !== "box box-filled-1") {
           event.target.classList.add("box-filled-2");
           player2.turn = false;
           player1.turn = true;
@@ -199,8 +200,8 @@ box.addEventListener("click", (event) => {
           player1.setActive();
           player2.setActive();
     }
-         console.log(gameBoard.boxCount);
-        gameBoard.stat;
+      ;
+        gameBoard.boardStatus;
         gameBoard.isGameFinished;
 
 });
